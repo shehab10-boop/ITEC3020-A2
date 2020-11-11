22167e0b0b4bdb4c92911e269e6dcc76b38d75b4
shehab10-boop <shehab_1297@hotmail.com>
2020-11-10 19:19:57 -0500

    Update ex1.js

--- A2/Ex1/ex1.js
+++ A2/Ex1/ex1.js
@@ -1,5 +1,27 @@
+
+
 function Ellipsoidvolume(a,b,c){
     return Math.pi * 4/3 * a * b *c;
+
 }
 
-function 
+function validator(){
+
+var a = document.getElementById("num1").content
+var b = document.getElementById("num2").content
+var c = document.getElementById("num3").content
+
+
+    if (a >= 1 && a <= 100){
+
+    }
+    
+    if (b >= 1 && b <=100){
+    
+    
+    }
+    
+    if (c >= 1 && c <= 100){
+    
+    }
+}
